8260ceae25ee2b686f6c160427f877855dda6ecc
Rhatahta <ryou.history476@gmail.com>
2022-07-14 23:04:56 +0900

    Add: ユーザーのログイン機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :people, only: %i[index show]
   resources :users, only: %i[new create]
   get 'static_pages/top'
+  get 'login' => 'user_sessions#new'
+  post 'login' => 'user_sessions#create'
+  delete 'logout' => 'user_sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
