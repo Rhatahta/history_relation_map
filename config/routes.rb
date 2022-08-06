978d907d0feb047aa2d6956fc617945c3470fe87
Rhatahta <ryou.history476@gmail.com>
2022-08-07 00:55:37 +0900

    Add: 管理者機能からlandmarksテーブルの編集ができるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     delete 'logout', to: 'user_sessions#destroy'
     resources :users
     resources :people
+    resources :landmarks
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
